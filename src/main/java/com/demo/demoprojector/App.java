42dd388ac1d01e389a40d449cfa3557291aae14e
Your Name <you@example.com>
2021-10-12 00:40:55 +0530

    initial commit

---
+++ src/main/java/com/demo/demoprojector/App.java
@@ -0,0 +1,13 @@
+package com.demo.demoprojector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
